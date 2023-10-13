f08c277f2da257024e124f3669f889e644132a9f
shey-kail <sheykail132@gmail.com>
2023-10-13 11:01:32 +0800

    add feature: preview in new buffer

--- lua/preview-R-nvim.lua
+++ lua/preview-R-nvim.lua
@@ -23,10 +23,7 @@ local check_pipe_file = function(pipe_file_path)
   end
 end
 
-function M.preview_tab()
-  -- 检查config.pipe_file_path是否存在，不存在则创建
-  check_pipe_file(config.pipe_file_path)
-  -- 先通过iron.nvim插件发送命令到R中，令R把要预览的数据写入管道文件(此时R进程阻塞)
+local R_send_to_pipe = function()
   require("iron.core").send(nil,
     string.format(
       "write.table(%s, quote = F, sep = \"\\t\", row.names = F, file = '%s')",
@@ -34,6 +31,18 @@ function M.preview_tab()
       config.pipe_file_path
     )
   )
+end
+
+function M.preview_newbuffer()
+  check_pipe_file(config.pipe_file_path)
+  R_send_to_pipe()
+  -- 新创建一个terminal的tab，执行预览命令
+  vim.cmd("terminal " .. preview_command)
+end
+
+function M.preview_tab()
+  check_pipe_file(config.pipe_file_path)
+  R_send_to_pipe()
   -- 新创建一个terminal的tab，执行预览命令
   vim.cmd("tabnew | terminal " .. preview_command)
 end
